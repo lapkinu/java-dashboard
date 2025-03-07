b17886adecace6246a5ac6352ef00ca2fc6bb150
lapkin <lapkinu@gmail.com>
2025-03-07 04:45:20 +0200

    added  ImageController

---
+++ src/main/java/example/neontest_db/controller/ImageController.java
@@ -0,0 +1,39 @@
+package example.neontest_db.controller;
+
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@RestController
+@RequestMapping("/api/images")
+public class ImageController {
+
+    private final String imageDirectory = "src/main/resources/static/customers/"; // Путь к директории с изображениями
+
+    @GetMapping("/{imageName}")
+    public ResponseEntity<Resource> getImage(@PathVariable String imageName) {
+        try {
+            Path file = Paths.get(imageDirectory).resolve(imageName);
+            Resource resource = new UrlResource(file.toUri());
+
+            if (resource.exists() || resource.isReadable()) {
+                return ResponseEntity.ok()
+                        .contentType(MediaType.IMAGE_PNG) // Укажите тип изображения
+                        .body(resource);
+            } else {
+                return ResponseEntity.notFound().build();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResponseEntity.internalServerError().build();
+        }
+    }
+}
