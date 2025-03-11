a7777cc90c0a8165d6047be91b0e0b680e86477d
lapkin <lapkinu@gmail.com>
2025-03-11 11:26:01 +0200

    change ImageController and application.properties

--- src/main/java/example/neontest_db/controller/ImageController.java
+++ src/main/java/example/neontest_db/controller/ImageController.java
@@ -1,8 +1,10 @@
 package example.neontest_db.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,6 +12,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -17,26 +22,52 @@ import java.nio.file.Paths;
 @RequestMapping("/api/images")
 public class ImageController {
 
-    @GetMapping("/**") // Изменяем endpoint для обработки любых путей после /api/images/
-    public ResponseEntity<Resource> getImage(HttpServletRequest request) { // Добавляем HttpServletRequest
-        try {
-            String requestURI = request.getRequestURI();
-            String imageName = requestURI.substring(requestURI.lastIndexOf("/") + 1); // Извлекаем имя файла
+    @Value("${image.directory:src/main/resources/static/customers/}")
+    private String imageDirectory;
 
-            String imageDirectory = "src/main/resources/static/customers/";
+    @GetMapping("/{imageName:.+}")
+    public ResponseEntity<Resource> getImage(@PathVariable String imageName) {
+        try {
             Path file = Paths.get(imageDirectory).resolve(imageName);
+            File f = file.toFile();
+            if (!f.exists()) {
+                return ResponseEntity.notFound().build();
+            }
+
             Resource resource = new UrlResource(file.toUri());
 
-            if (resource.exists() || resource.isReadable()) {
-                return ResponseEntity.ok()
-                        .contentType(MediaType.IMAGE_PNG) // Укажите тип изображения
-                        .body(resource);
+            if (resource.exists() && resource.isReadable()) {
+                MediaType contentType = determineContentType(file);
+
+                if (contentType != null) {
+                    HttpHeaders headers = new HttpHeaders();
+                    headers.setContentType(contentType);
+
+                    return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+                } else {
+                    return ResponseEntity.ok()
+                            .contentType(MediaType.APPLICATION_OCTET_STREAM) // Тип по умолчанию
+                            .body(resource);
+                }
+
             } else {
                 return ResponseEntity.notFound().build();
             }
-        } catch (Exception e) {
+        } catch (IOException e) {
             e.printStackTrace();
             return ResponseEntity.internalServerError().build();
         }
+    }
+
+    private MediaType determineContentType(Path file) {
+        try {
+            String contentType = Files.probeContentType(file);
+            if (contentType != null) {
+                return MediaType.parseMediaType(contentType);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 }
